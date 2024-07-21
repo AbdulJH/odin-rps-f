fee3f1c61d6bb15dfa0d8f809044300c68720b1d
Abdul Hussein <abdul.chs21@gmail.com>
2024-07-21 15:15:33 -0500

    made getUserChoice function.

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * options.length);
   return options[randomIndex];
 }
+
+function getUserChoice() {
+  const userPrompt = prompt("ROCK , PAPER, OR SCISSORS?").toLowerCase();
+  if (
+    userPrompt !== "rock" &&
+    userPrompt !== "paper" &&
+    userPrompt !== "scissors"
+  ) {
+    alert("ERROR!! MUST PICK A VALID CHOICE");
+    return getUserChoice();
+  }
+
+  return userPrompt;
+}
+
+console.log(getComputerChoice());
+console.log(getUserChoice());
